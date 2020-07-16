17c6a366821d4b5b215d2e4daca6df21ffc4108b
PAPPY-JOE <fatoyejoseph@gmail.com>
2020-07-16 13:56:09 +0100

    Added files

---
+++ javascript/index.js
@@ -0,0 +1,19 @@
+// document.body.innerHTML = "Jesus is Lord!";
+
+var names = "Daniel";
+alert(names);
+
+
+// var testExample = function () {
+//     var greeting = "Hi! My name is " + a;
+//     return greeting;
+// }
+// var a = "Joseph";
+// document.body.innerHTML = testExample(a);
+
+
+// (function () {
+//     var greeting = "Hi! my name is Joseph";
+//     console.log(greeting);
+//     document.body.innerHTML = greeting;
+// }())
